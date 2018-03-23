88eb5ce664af31d9bd6893c52dff3542b1715860
Michi2806 <michi2806@gmail.com>
2018-03-23 09:01:16 +0100

    Now App.js is realy cleaned up

--- spoti-vote/src/App.js
+++ spoti-vote/src/App.js
@@ -1,15 +1,5 @@
 import React, {Component} from 'react';
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import {faAngleDown} from '@fortawesome/fontawesome-free-solid';
 import './css/App.css';
-
-let defaultStyle = {
-	position: 'absolute',
-	marginLeft: 'auto',
-	marginRight: 'auto',
-	marginTop: 'auto',
-	color: '#191414'
-}
 
 class App extends Component {
 
