77447fe550eedf08df5a4e193dd440637daf04dc
Gabsii <lukas.gabsi@tele2.at>
2018-03-27 18:12:28 +0200

    YOU SHALL NOT PASS

--- spoti-vote/src/components/App.jsx
+++ spoti-vote/src/components/App.jsx
@@ -2,12 +2,18 @@ import React, {Component} from 'react';
 import Footer from './Footer.jsx';
 import Sidebar from './Sidebar/Sidebar.jsx';
 import CardContainer from './Cards/CardContainer.jsx';
+import queryString from 'query-string';
 
 let color = require('../css/colors.js');
 
 class App extends Component {
+	componentDidMount() {
+		const access_token = queryString.parse(window.location.search).access_token;
+		if (!access_token) {
+			window.location = "http://localhost:3000/";
+		}
+	}
 	render() {
-
 		return (<section style={{
 				backgroundColor: color.background,
 				height: '100vh',
