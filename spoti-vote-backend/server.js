bcd56276ae4b574cf4ee07934e2ae6d0d56802ba
Michiocre <michi2806@gmail.com>
2018-04-11 23:19:08 +0200

    I think this is it

--- spoti-vote-backend/server.js
+++ spoti-vote-backend/server.js
@@ -156,7 +156,7 @@ io.on('connection', function(socket) {
 		allClients.splice(i,1);
 		if (allClients.length >= 0 && rooms.length > 0) {
 			//rooms = [];
-			console.log('All rooms were deleted');
+			//console.log('All rooms were deleted');
 		}
     });
 });
