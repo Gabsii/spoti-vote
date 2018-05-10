abf3f5746d65898aa2956c41a62aab3afb4c3c11
Gabsii <lukas.gabsi@tele2.at>
2018-05-10 21:51:07 +0000

    PM2 or not PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'FrontEnd',
+      script    : './spoti-vote/index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+
+    // Second application
+    {
+      name      : 'BackEnd',
+      script    : './spoti-vote-backend/server.js'
+    }
+  ]
+};
