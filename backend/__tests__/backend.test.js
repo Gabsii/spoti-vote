032ef0251dd7ce5a8743c64c970ab004b8dfba06
Michi2806 <michi2806@gmail.com>
2019-06-28 10:33:12 +0200

    More Backend Testing

--- backend/__tests__/backend.test.js
+++ backend/__tests__/backend.test.js
@@ -12,6 +12,15 @@ const rooms = require('../app').rooms;
 
 users.push(new User('', '', '', ''));
 users[0].id = 'User1';
+users[0].name = 'Max';
+
+users.push(new User('', '', '', ''));
+users[1].id = 'Gabsii';
+users[1].name = 'Gabsii';
+
+users.push(new User('', '', '', ''));
+users[2].id = 'Michiocre';
+users[2].name = 'Michiocre';
 
 dotenv.config();
 const ipAddress = process.env.ADDRESS;
@@ -21,129 +30,9 @@ const portBack = process.env.PORTBACK;
 const uriBack = (ipAddress === 'localhost' ? 'http://' + ipAddress + ':' + portBack : 'https://' + ipAddress + ':' + port);
 const redirect_uri = uriBack + '/callback';
 
-describe('Basic Functions Tests', () => {
-	describe('getRoomById', () => {
-		test('The requested room is returned', () => {
-			let rooms = [
-				{id: 'QUSDN'},
-				{id: 'UADBA'},
-				{id: 'DAJDN'},
-				{id: 'WNIFP'},
-				{id: 'YXNCB'}
-			];
-			expect(lib.getRoomById('UADBA', rooms)).toBe(rooms[1]);
-			expect(lib.getRoomById('WNIFP', rooms)).toBe(rooms[3]);
-			expect(lib.getRoomById('YXNCB', rooms)).toBe(rooms[4]);
-		});
-	});
-	
-	describe('getUserById', () => {
-		test('The requested User', () => {
-			let users = [
-				{id: 'blankesMichl'},
-				{id: 'Gabsii'},
-				{id: 'someoneElse'},
-				{id: 'AnotherOPne'},
-				{id: 'User'}
-			];
-			expect(lib.getUserById('Gabsii', users)).toBe(users[1]);
-			expect(lib.getUserById('blankesMichl', users)).toBe(users[0]);
-			expect(lib.getUserById('User', users)).toBe(users[4]);
-		});
-	});
-
-	describe('makeId', () => {
-		test('If the id has the right length', () => {
-			let id = makeid(5);
-			expect(id).toHaveLength(5);
-		});
-		
-		test('If a unique id is returned every time', () => {
-			let ids = [];
-			for (let i = 0; i < 26; i++) {
-				let newId = makeid(5);
-				expect(ids.includes(newId)).toBe(false);
-				if (ids.includes(ids[i])) {
-					break;
-				}
-				ids.push(newId);
-			}
-		});
-	});
-});
-
-describe('Room Functions Tests', () => {
-	let testUser = new User();
-	testUser.name = 'Max Mustermann';
-	testUser.id = 'maxmuster';
-
-	let testRoom = new Room(testUser, []);
-	testRoom.connectedUser = [
-		{name: 'TestUser1'},
-		{name: 'TestUser2'},
-		{name: 'TestUser3'}
-	];
-
-	test('Room generation', () => {
-		expect(testRoom.id).toHaveLength(5);
-	});
-	test('Get User Names', () => {
-		expect(testRoom.getUserNames()).toContain(testRoom.connectedUser[1].name, testUser.name);
-		expect(testRoom.getUserNames()).toHaveLength(4);
-	});
-	test('Get User by Name', () => {
-		expect(testRoom.getUserByName('Max Mustermann')).toBe(testUser);
-		expect(testRoom.getUserByName('TestUser2')).toBe(testRoom.connectedUser[1]);
-		expect(testRoom.getUserByName('Not Existent User')).toBeNull();
-	});
-	test('Add User', () => {
-		expect(testRoom.addUser('TempUser')).toBeTruthy();
-		expect(testRoom.getUserNames()).toContain('TempUser');
-		expect(testRoom.getUserNames()).toHaveLength(5);
-	});
-	test('Remove User', () => {
-		expect(testRoom.removeUser('TempUser')).toEqual({name: 'TempUser', voted: null});
-		expect(testRoom.getUserNames()).not.toContain('TempUser');
-		expect(testRoom.getUserNames()).toHaveLength(4);
-		expect(testRoom.removeUser('Not Existent User')).toBeNull();
-	});
-
-	test('Get Playlist by Id', () => {
-		testRoom.user.playlists = [
-			{id: 'playlist1'},
-			{id: 'playlist2'},
-			{id: 'playlist3'},
-			{id: 'playlist4'}
-		];
-
-		expect(testRoom.getPlaylistById('playlist3')).toBe(testRoom.user.playlists[2]);
-		expect(testRoom.getPlaylistById('Not Existent Playlist')).toBeNull();
-	});
-
-	test('Get Active Track by Id', () => {
-		testRoom.activeTracks = [
-			{id: 'activeTrack1'},
-			{id: 'activeTrack2'},
-			{id: 'activeTrack3'},
-			{id: 'activeTrack4'}
-		];
-
-		expect(testRoom.getActiveTrackById('activeTrack4')).toBe(testRoom.activeTracks[3]);
-		expect(testRoom.getActiveTrackById('Not Existent Track')).toBeNull();
-	});
-
-	test('Difference Calculator First Iteration', () => {
-		let oldTestRoom = null;
-		testRoom.activeTracks = [];
-		testRoom.user.voted = 'track1';
-		testRoom.user.img = 'https://upload.wikimedia.org/wikipedia/en/f/f9/Dat_Boi_%28resized_50%25%29.jpg';
-
-		let update = testRoom.getDifference(oldTestRoom);
-		expect(update.host).toEqual({name: 'Max Mustermann', voted: 'track1', img: 'https://upload.wikimedia.org/wikipedia/en/f/f9/Dat_Boi_%28resized_50%25%29.jpg'});
-	});
-});
-
 describe('App Tests', () => {
+	let roomIds  = [];
+
 	describe('Get / Method', () => {
 		test('It should responde with a little greeting', (done) =>  {
 			request(server).get('/').then((response) => {
@@ -192,10 +81,155 @@ describe('App Tests', () => {
 		test('It should responde with a redirect', (done) =>  {
 			request(server).get('/createRoom?id=User1').then((response) => {
 				expect(response.statusCode).toBe(302);
-				
+				roomIds.push(response.headers.location.split('/').pop());
+				expect(rooms.length > 0).toBeTruthy();
+				done();
+			});
+
+			request(server).get('/createRoom?id=Gabsii').then((response) => {
+				expect(response.statusCode).toBe(302);
+				roomIds.push(response.headers.location.split('/').pop());
+				expect(rooms.length > 0).toBeTruthy();
+				done();
+			});
+	
+			request(server).get('/createRoom?id=Michiocre').then((response) => {
+				expect(response.statusCode).toBe(302);
+				roomIds.push(response.headers.location.split('/').pop());
 				expect(rooms.length > 0).toBeTruthy();
 				done();
 			});
 		});
 	});
+
+	describe('Get /rooms Method', () => {
+		test('It should responde with a list of all rooms', (done) =>  {
+			request(server).get('/rooms').then((response) => {
+				expect(response.statusCode).toBe(200);
+				let body = JSON.parse(response.text);
+				expect(body.content.length).toHaveLength(rooms.length);
+				for (let i = body.content.length - 1; i > body.content.length - roomIds.length; i--) {
+					expect(body.content[i].roomName).toBe(roomIds.pop());
+				}
+				done();
+			});
+		});
+	});
 });
+
+describe('Basic Functions Tests', () => {
+	let testRooms = [];
+
+	testRooms.push(new Room(users[0], testRooms));
+	testRooms.push(new Room(users[1], testRooms));
+	testRooms.push(new Room(users[2], testRooms));
+
+	describe('getRoomById', () => {
+		test('The requested room is returned', () => {
+			expect(lib.getRoomById('UADBA', testRooms)).toBe(null);
+			for (let i = 0; i < testRooms.length; i++) {
+				expect(lib.getRoomById(testRooms[0].id, testRooms)).not.toBeNull();
+			expect(lib.getRoomById(testRooms[0].id, testRooms)).toBe(rooms[0]);
+			expect(lib.getRoomById(testRooms[1].id, testRooms)).not.toBeNull();
+			expect(lib.getRoomById(testRooms[1].id, testRooms)).toBe(rooms[i]);
+			expect(lib.getRoomById(testRooms[2].id, testRooms)).not.toBeNull();
+			expect(lib.getRoomById(testRooms[2].id, testRooms)).toBe(rooms[i]);
+				
+			}
+		});
+	});
+	
+	describe('getUserById', () => {
+		test('The requested User', () => {
+			expect(lib.getUserById('blankesMichl', users)).toBe(null);
+			expect(lib.getUserById('User1', users)).toBe(users[0]);
+			expect(lib.getUserById('Michiocre', users)).toBe(users[2]);
+			expect(lib.getUserById('Gabsii', users)).toBe(users[1]);
+		});
+	});
+
+	describe('makeId', () => {
+		test('If the id has the right length', () => {
+			let id = makeid(5);
+			expect(id).toHaveLength(5);
+		});
+	});
+});
+
+describe('Room Functions Tests', () => {
+	let testRooms = [];
+
+	testRooms.push(new Room(users[0], testRooms));
+	testRooms.push(new Room(users[1], testRooms));
+	testRooms.push(new Room(users[2], testRooms));
+
+	testRooms.forEach(testRoom => {
+		let testUser = testRoom.user;
+		
+		test('Room generation', () => {
+			expect(testRoom.id).toHaveLength(5);
+		});
+	
+		test('Add User', () => {
+			expect(testRoom.addUser('TempUser0')).toBeTruthy();
+			expect(testRoom.addUser('TempUser1')).toBeTruthy();
+			expect(testRoom.addUser('TempUser2')).toBeTruthy();
+			expect(testRoom.addUser('TempUser3')).toBeTruthy();
+			expect(testRoom.getUserNames()).toContain('TempUser2');
+			expect(testRoom.getUserNames()).toHaveLength(5);
+		});
+
+		test('Get User Names', () => {
+			expect(testRoom.getUserNames()).toContain('TempUser0', 'TempUser1', 'TempUser2', 'TempUser3', testUser.name);
+			expect(testRoom.getUserNames()).toHaveLength(5);
+		});
+	
+		test('Get User by Name', () => {
+			expect(testRoom.getUserByName('TempUser1')).toBe(testRoom.connectedUser[1]);
+			expect(testRoom.getUserByName(testUser.name)).toBe(testRoom.user);
+			expect(testRoom.getUserByName('Not Existent User')).toBeNull();
+		});
+
+		test('Remove User', () => {
+			expect(testRoom.removeUser('TempUser0')).toEqual({name: 'TempUser0', voted: null});
+			expect(testRoom.getUserNames()).not.toContain('TempUser0');
+			expect(testRoom.getUserNames()).toHaveLength(4);
+			expect(testRoom.removeUser('Not Existent User')).toBeNull();
+		});
+	
+		test('Get Playlist by Id', () => {
+			testRoom.user.playlists = [
+				{id: 'playlist1'},
+				{id: 'playlist2'},
+				{id: 'playlist3'},
+				{id: 'playlist4'}
+			];
+	
+			expect(testRoom.getPlaylistById('playlist3')).toBe(testRoom.user.playlists[2]);
+			expect(testRoom.getPlaylistById('Not Existent Playlist')).toBeNull();
+		});
+	
+		test('Get Active Track by Id', () => {
+			testRoom.activeTracks = [
+				{id: 'activeTrack1'},
+				{id: 'activeTrack2'},
+				{id: 'activeTrack3'},
+				{id: 'activeTrack4'}
+			];
+	
+			expect(testRoom.getActiveTrackById('activeTrack4')).toBe(testRoom.activeTracks[3]);
+			expect(testRoom.getActiveTrackById('Not Existent Track')).toBeNull();
+		});
+	
+		test('Difference Calculator First Iteration', () => {
+			let oldTestRoom = null;
+			testRoom.activeTracks = [];
+			testRoom.user.voted = 'track1';
+			testRoom.user.img = 'https://upload.wikimedia.org/wikipedia/en/f/f9/Dat_Boi_%28resized_50%25%29.jpg';
+	
+			let update = testRoom.getDifference(oldTestRoom);
+			expect(update.host).toEqual({name: testUser.name, voted: 'track1', img: 'https://upload.wikimedia.org/wikipedia/en/f/f9/Dat_Boi_%28resized_50%25%29.jpg'});
+		});
+	});
+});
+
