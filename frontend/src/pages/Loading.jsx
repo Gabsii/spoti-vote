3d6f1a6b514e18878bafeeac5507a9b20941ed01
Gabsii <lukas.gabsi@tele2.at>
2019-09-01 11:24:04 +0200

    Updated Loading Screen

--- frontend/src/pages/Loading.jsx
+++ frontend/src/pages/Loading.jsx
@@ -1,8 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Spinner from '../components/Dashboard/Main/Spinner.jsx';
+
+const constants = require('../js/constants');
 
 class Loading extends Component {
     render() {
-        return (<div>Loading...</div>);
+        return ( <div style={
+            {
+                width: '100vw',
+                height: '100vh',
+                backgroundColor: constants.colors.backgroundLite,
+                color: constants.colors.font,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }
+        }><Spinner/></div>);
     }
 }
 export default Loading;
