85d9bc72fe7569daa47686a1b0b0c77776aa7c5b
Michi2806 <michi2806@gmail.com>
2019-10-17 21:45:00 +0200

    Sorry hound

--- frontend/config/pnpTs.js
+++ frontend/config/pnpTs.js
@@ -1,4 +1,4 @@
-//'use strict';
+'use strict';
 
 const { resolveModuleName } = require('ts-pnp');
 
--- frontend/scripts/start.js
+++ frontend/scripts/start.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-//'use strict';
+'use strict';
 
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
