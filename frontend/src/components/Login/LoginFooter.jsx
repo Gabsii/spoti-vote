c0e3c5bc46f4fa525818646aec85ef0ae7ee9cb8
Sanket <2016bcs016@sggs.ac.in>
2019-10-22 22:25:50 +0530

    #129 Fixed footer issue

--- frontend/src/components/Login/LoginFooter.jsx
+++ frontend/src/components/Login/LoginFooter.jsx
@@ -11,19 +11,27 @@ const styles = {
         textOverflow: 'ellipsis',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        flexDirection: 'column',
+        '& > div': { margin: '0.25rem' }
     }),
     link: css({color: 'white', textDecoration: 'none'})
 };
 
 const LoginFooter = () => {
 
-    return (<footer className={`${styles.footer}`}>Help us to keep Spoti-Vote running
-        <br/>
-        &copy; Lukas Gabsi, Michael Blank
-        <br/>
-        <a href="/policies" className={`${styles.link}`}>Policies</a>
-    </footer>);
+    return(
+        <footer className={`${styles.footer}`}>
+            <div>
+                Help us to keep Spoti-Vote running
+            </div>
+            <div>
+                &copy; Lukas Gabsi, Michael Blank
+            </div>
+            <div>
+                <a href="/policies" className={`${styles.link}`}>Policies</a>
+            </div>
+        </footer>);
 };
 
 export default LoginFooter;
