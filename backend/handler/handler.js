fd1d9ddece797d45006eb24765d610440ab874d2
Michiocre <michi2806@gmail.com>
2020-10-03 01:00:45 +0200

    Labeler Fix

--- backend/handler/handler.js
+++ backend/handler/handler.js
@@ -2,7 +2,6 @@ const dotenv = require('dotenv');
 const fs = require('fs');
 const Host = require('./Classes/Host');
 const Room = require('./Classes/Room');
-const { PassThrough } = require('stream');
 
 const dataLoc = 'handler/data.json';
 
@@ -14,7 +13,7 @@ function getData() {
     try {
         parsed = JSON.parse(fs.readFileSync(dataLoc));
     } catch (error) {
-        console.error("data.json file had to be created.");
+        console.error('data.json file had to be created.');
     }
 
     let hosts = [];
