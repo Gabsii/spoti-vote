081345f8c77dbbf6648154d9d78b91ece7083029
Michiocre <michi2806@gmail.com>
2020-10-05 22:34:36 +0200

    Why

--- backend/__tests__/testingSpotifyApi.js
+++ backend/__tests__/testingSpotifyApi.js
@@ -10,6 +10,8 @@ let data = handler.getTestData();
 app.get('/accounts/authorize', (req, res) => {
     if(req.query.response_type === 'code' && req.query.client_id === process.env.SPOTIFY_CLIENT_ID) {
         res.send(req.query.redirect_uri + '?code:' + data.tokens[0]);
+    } else {
+        res.status(400).send();
     }
 });
 
