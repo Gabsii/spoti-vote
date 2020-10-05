d8e67a495c6ceafa53bdf9df90a7b0fb493b02be
Michiocre <michi2806@gmail.com>
2020-10-05 22:40:18 +0200

    Yahooo found it

--- backend/__tests__/testingSpotifyApi.js
+++ backend/__tests__/testingSpotifyApi.js
@@ -8,6 +8,7 @@ const server = http.createServer(app);
 let data = handler.getTestData();
 
 app.get('/accounts/authorize', (req, res) => {
+    handler.log(req.query);
     if(req.query.response_type === 'code' && req.query.client_id === process.env.SPOTIFY_CLIENT_ID) {
         res.send(req.query.redirect_uri + '?code:' + data.tokens[0]);
     } else {
